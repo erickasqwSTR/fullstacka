64e8bd153af22d662202af16e37d9defab8df785
fullstacktest <fullstacktest@example.com>
2026-06-04 18:04:28 -0500

    agregar dependencia morgan faltante

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,4 @@
 module.exports = {
-  development: {
-    client: 'mysql2',
-    connection: {
-      host: 'br0espw1omg8txsgewqo-mysql.services.clever-cloud.com',
-      user: 'umqqbnhtjorfax7o',
-      password: 'pfYUe9XqFuD5uIvULO6h',
-      database: 'br0espw1omg8txsgewqo',
-      port: 3306
-    }
-  },
   production: {
     client: 'mysql2',
     connection: {
@@ -17,6 +7,9 @@ module.exports = {
       password: 'pfYUe9XqFuD5uIvULO6h',
       database: 'br0espw1omg8txsgewqo',
       port: 3306
+    },
+    migrations: {
+      directory: './migrations'
     }
   }
 };

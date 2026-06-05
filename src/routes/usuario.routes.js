98c6dd983e9d59432784dc61aade7c08240ed561
fullstacktest <fullstacktest@example.com>
2026-06-04 21:05:06 -0500

    Agregar soporte PUT para actualizar usuario

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -5,5 +5,6 @@ const usuarioController = require("../controllers/usuario.controller");
 router.get("/", usuarioController.listarUsuarios);
 router.post("/", usuarioController.insertarUsuario);
 router.patch("/:id", usuarioController.actualizarUsuario);
+router.put("/:id", usuarioController.actualizarUsuario);
 
 module.exports = router;
